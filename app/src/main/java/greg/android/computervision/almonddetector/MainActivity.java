645cef36dfb9496db262ad5e26a402efce0c8990
Gregor von Gunten <gregor.vongunten@fhnw.ch>
2018-06-15 11:58:34 +0200

    addad load image in photoview funcionality

--- app/src/main/java/greg/android/computervision/almonddetector/MainActivity.java
+++ app/src/main/java/greg/android/computervision/almonddetector/MainActivity.java
@@ -1,7 +1,10 @@
 package greg.android.computervision.almonddetector;
 
 import android.content.Intent;
-import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String modelPath = "frozen_inference_graph.pb";
 
     private Button openGalleryButton;
+    private ImageView photoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         openGalleryButton = findViewById(R.id.myGalleryButton);
+        photoView = findViewById(R.id.myPhotoView);
 
         setModel();
 
@@ -58,6 +67,37 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(myIntent,100);
     }
 
+    private Bitmap getBitmapFromUri(Uri uri) {
+        Bitmap bitmap;
+
+        try {
+            ParcelFileDescriptor parcelFileDescriptor =
+                    getContentResolver().openFileDescriptor(uri, "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+            parcelFileDescriptor.close();
+        } catch (IOException e) {
+            Log.e("Exception!!", "Failed to get bitmap from uri", e);
+            return null;
+        }
+        return bitmap;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            final Bitmap bitmap;
+            bitmap = getBitmapFromUri(data.getData());
+
+            photoView.setImageBitmap(bitmap);
 
 
+
+
+
+
+        }
+
+
+        }
 }
